2abd5812724deba9b00da8898a42f6fc758de1bc
HannonN <nathan22hannon@gmail.com>
2021-02-19 16:05:42 -0500

    routing-unfinished

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  appKey: string = 'c0bf23a0689bd3e32d80dd41405a2e8a';
+  edamamSearchUrl: string = 'https://api.edamam.com/search';
+  favorites: any[] = [];
+  // enum CuisineType{
+  //   indian:"indian",
+  // }
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRecipes = (): any => {
+    return this.http.get(this.edamamSearchUrl, {
+      params: {
+        appKey: this.appKey,
+      },
+    });
+  };
+
+  searchRecipes = (
+    searchTerm: string,
+    cookTime: string,
+    cuisine: string
+  ): any => {
+    return this.http.get(this.edamamSearchUrl, {
+      params: {
+        appKey: this.appKey,
+        q: searchTerm,
+        time: cookTime,
+        cuisineType: cuisine,
+      },
+    });
+  };
 }
