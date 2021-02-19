1bb3cf405d9f8ed5fa76bfc472f866fb5bf71e59
Spenn0 <sabcompositions@gmail.com>
2021-02-19 14:58:59 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HeaderComponent } from './header/header.component';
+import { Search1Component } from './search1/search1.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavoritesComponent,
+    FourOhFourComponent,
+    HeaderComponent,
+    Search1Component,
+    RecipeCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
