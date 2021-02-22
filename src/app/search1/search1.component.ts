202022f04692900eae914ac3209dadd989149d32
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2021-02-22 10:45:06 -0500

    progress made on css and js

--- src/app/search1/search1.component.ts
+++ src/app/search1/search1.component.ts
@@ -1,3 +1,4 @@
+import { formatCurrency } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -13,8 +14,47 @@ export class Search1Component implements OnInit {
 
   ngOnInit(): void {}
 
+// let noDietForm = {
+//     this.form.value.cookTime,
+//     this.form.value.searchQuery,
+//   }
+//   let noTimeForm = {
+//     this.form.value.searchQuery,
+//     this.form.value.dietType
+//   }
+//   let noTimeOrDietForm = {
+//     this.form.value.searchQuery
+//   }
+
+  emitNoDietForm = (form: NgForm) => {
+    this.searchEvent.emit(form.value.searchQuery);
+    this.searchEvent.emit(form.value.cookTime);
+  }
+  emitNoTimeForm = (form: NgForm) => {
+    this.searchEvent.emit(form.value.searchQuery);
+    this.searchEvent.emit(form.value.dietType);
+  }
+  emitNoTimeOrDietForm = (form: NgForm) => {
+    this.searchEvent.emit(form.value.searchQuery);
+  }
+
   emitSearchTerm = (form: NgForm) => {
-    console.log(form);
-    this.searchEvent.emit(form);
-  };
+
+    if (form.form.value.dietType === "" && form.form.value.cookTime ==! ""){
+      this.emitNoDietForm(form);
+    } else if (form.form.value.cookTime === "" && form.form.value.dietType ==! ""){
+      this.emitNoTimeForm(form);
+    } else if (form.form.value.dietType === "" && form.form.value.cookTime === ""){
+      this.emitNoTimeOrDietForm(form);
+    } else {
+          this.searchEvent.emit(form);
+    }
+  }
+
+    // emitSearchTerm = (form: NgForm) => {
+  //   console.log(form);
+  //   this.searchEvent.emit(form);
+  // };
+
+
 }
